0a3e97c941accac4020bd1c6fb258750f4a5a8dc
damon kang <38835104+gloomydumber@users.noreply.github.com>
2019-07-01 20:12:07 +0900

    Add files via upload

---
+++ p0141.cpp
@@ -0,0 +1,11 @@
+#include "p014.h"
+
+void BestComImpl::SimpleFunc(void)
+{
+  std::cout<<"BestCom defined this function."<<std::endl;
+}
+
+void ProgComImpl::SimpleFunc(void)
+{
+  std::cout<<"ProgCom defined this function."<<std::endl;
+}
---
+++ p0142.cpp
@@ -0,0 +1,9 @@
+#include "p014.h"
+#include "p0141.cpp"
+
+int main(void)
+{
+  BestComImpl::SimpleFunc();
+  ProgComImpl::SimpleFunc();
+  return 0;
+}
